5b0875a73decc8d83aa1063c3c9ae431ef6d69c9
gautam625-hub <gautamsahota625@gmail.com>
2025-07-04 12:48:48 +0000

    changes

--- src/mcqgenerator/mcqgenerator.py
+++ src/mcqgenerator/mcqgenerator.py
@@ -40,3 +40,24 @@ quiz_generation_prompt = PromptTemplate(
 
 
 quiz_chain=LLMChain(llm=llm,prompt=quiz_generation_prompt,output_key="quiz",verbose=True)
+
+template2="""
+You are an expert english grammarian and writer. Given a Multiple Choice Quiz for {subject} students.\
+You need to evaluate the complexity of the question and give a complete analysis of the quiz. Only use at max 50 words for complexity analysis. 
+if the quiz is not at per with the cognitive and analytical abilities of the students,\
+update the quiz questions which needs to be changed and change the tone such that it perfectly fits the student abilities
+Quiz_MCQs:
+{quiz}
+
+Check from an expert English Writer of the above quiz:
+"""
+
+
+quiz_evaluation_prompt=PromptTemplate(input_variables=["subject", "quiz"], template=template2)
+
+review_chain=LLMChain(llm=llm, prompt=quiz_evaluation_prompt, output_key="review", verbose=True)
+
+
+# This is an Overall Chain where we run the two chains in Sequence
+generate_evaluate_chain=SequentialChain(chains=[quiz_chain, review_chain], input_variables=["text", "number", "subject", "tone", "response_json"],
+                                        output_variables=["quiz", "review"], verbose=True,)
