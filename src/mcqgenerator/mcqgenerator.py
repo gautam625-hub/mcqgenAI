5bd2b8fe9ddd136dcdecfa98a92b39917776bd17
gautam625-hub <gautamsahota625@gmail.com>
2025-07-04 12:43:01 +0000

    mcqgen

--- src/mcqgenerator/mcqgenerator.py
+++ src/mcqgenerator/mcqgenerator.py
@@ -21,3 +21,22 @@ key=os.getenv("OPENAI_API_KEY")
 
 
 llm = ChatOpenAI(openai_api_key=key,model_name="gpt-3.5-turbo", temperature=0.7)
+
+template="""
+Text:{text}
+You are an expert MCQ maker. Given the above text, it is your job to \
+create a quiz  of {number} multiple choice questions for {subject} students in {tone} tone. 
+Make sure the questions are not repeated and check all the questions to be conforming the text as well.
+Make sure to format your response like  RESPONSE_JSON below  and use it as a guide. \
+Ensure to make {number} MCQs
+### RESPONSE_JSON
+{response_json}
+
+"""
+
+quiz_generation_prompt = PromptTemplate(
+    input_variables=["text", "number", "subject", "tone", "response_json"],
+    template=template)
+
+
+quiz_chain=LLMChain(llm=llm,prompt=quiz_generation_prompt,output_key="quiz",verbose=True)
