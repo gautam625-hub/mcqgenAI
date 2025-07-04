8c99c98506e2d8a00c84ce31d4f319024f3ce5b3
gautam625-hub <gautamsahota625@gmail.com>
2025-07-04 16:13:29 +0000

    changes made

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -3,8 +3,8 @@ import json
 import traceback
 import pandas as pd
 from dotenv import load_dotenv
-from src.mcqgenerator.utils import read_file, get_table_data
+from src.mcqgenerator.utlis import read_file,get_table_data
 import streamlit as st
-from langchain.callbacks import get_openai_callback
-from src.mcqgenerator.MCQgenerator import generate_evaluate_chain
+from langchain_community.callbacks.manager import get_openai_callback
+# from src.mcqgenerator.MCQgenerator import generate_evaluate_chain
 from src.mcqgenerator.logger import logging
