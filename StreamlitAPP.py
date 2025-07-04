b52e074b04aa63e649df739a2c31b3b1adee2231
gautam625-hub <gautamsahota625@gmail.com>
2025-07-04 15:03:43 +0000

    changes

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -3,8 +3,8 @@ import json
 import traceback
 import pandas as pd
 from dotenv import load_dotenv
-from mcqgenerator.utils import read_file, get_table_data
+from src.mcqgenerator.utils import read_file, get_table_data
 import streamlit as st
 from langchain.callbacks import get_openai_callback
-from mcqgenerator.MCQgenerator import generator_evaluate_chain
-from mcqgenerator.logger import logging
+from src.mcqgenerator.MCQgenerator import generator_evaluate_chain
+from src.mcqgenerator.logger import logging
