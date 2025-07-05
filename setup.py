a4bc887e1f2f034e1386c4e6fd6b5191865bb38f
gautam625-hub <gautamsahota625@gmail.com>
2025-07-05 10:05:31 +0000

    changes

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='gautam sahota',
     author_email='gautamsahota625@gmail.com',
-    install_requires=['openai','langchain','streamlit','python-dotenv','PyPDF2''langchain-community','tiktoken'],
+    install_requires=['openai','langchain','streamlit','python-dotenv','PyPDF2','langchain-community','tiktoken'],
     packages=find_packages()
 )
